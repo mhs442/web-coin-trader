a3fe9dc5770c180b4fbf0ac0636f54b21d3a95b0
moon <bks442@naver.com>
2026-03-14 00:28:47 +0900

    refactor(common): CustomException에 로그 추가

--- src/main/java/com/coin/webcointrader/common/exception/CustomException.java
+++ src/main/java/com/coin/webcointrader/common/exception/CustomException.java
@@ -2,18 +2,22 @@ package com.coin.webcointrader.common.exception;
 
 import com.coin.webcointrader.common.enums.ExceptionMessage;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
+@Slf4j
 public class CustomException extends RuntimeException {
     private final ExceptionMessage exceptionMessage;
 
     public CustomException(ExceptionMessage exceptionMessage) {
         super(exceptionMessage.getMessage());
         this.exceptionMessage = exceptionMessage;
+        log.error(exceptionMessage.getMessage());
     }
 
     public CustomException(ExceptionMessage exceptionMessage, Throwable cause) {
         super(exceptionMessage.getMessage(), cause);
         this.exceptionMessage = exceptionMessage;
+        log.error(exceptionMessage.getMessage(), cause);
     }
 }
